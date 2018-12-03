4dbbc70a10ed772cc31f5575b9df7774084cb439
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2018-12-03 20:52:29 -0300

    Socket.io test page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,17 @@
+const path = require('path');
+
 module.exports = (app, io) => {
   const healthController = require('../controllers/health');
   const notificationsController = require('../controllers/notifications')(io);
 
   app.get('/notifications-service/health', healthController.health);
   app.post('/notifications/send', notificationsController.send);
+  app.get('/notifications-service/test', (req, res) => {
+    res.sendFile(path.join(__dirname+'/../tests/index.html'));
+  });
 
   app.get('/notifications/:hash', notificationsController.get);
 
   app.delete('/notifications/:hash/:id', notificationsController.remove);
+
 };

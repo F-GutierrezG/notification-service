19363f0986c5f22c399384d102d8ba991e59b86a
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2018-12-03 17:20:37 -0300

    FIX: Deploy script

--- stage-deploy.sh
+++ stage-deploy.sh
@@ -17,4 +17,4 @@ ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d -e 'NODE_E
 
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network connect onelike-network --ip 172.18.0.12 notifications'
 
-docker container exec -i notifications bash -c "cd src && ../node_modules/.bin/sequelize db:migrate"
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker container exec -i notifications bash -c "cd src && ../node_modules/.bin/sequelize db:migrate"'

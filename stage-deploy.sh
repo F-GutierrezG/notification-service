47c2483f919306003e4a1cf1955e80cb56bac1bb
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2018-12-04 15:30:53 -0300

    Add container restart policy

--- stage-deploy.sh
+++ stage-deploy.sh
@@ -12,8 +12,8 @@ ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker container rm noti
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker image rm $(docker images registry.gitlab.com/gusisoft/onelike/notifications-service -q)'
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker image rm $(docker images registry.gitlab.com/gusisoft/onelike/notifications-db -q)'
 
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d -e 'POSTGRES_USER=postgres' -e 'POSTGRES_PASSWORD=postgres' -p 5436:5432 --name notifications-db --network notifications-service-network --ip 172.23.0.3 $REGISTRY_REPO/$NOTIFICATIONS_DB:$TAG"
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d -e 'NODE_ENV=development' -e 'DATABASE_URL=postgres://postgres:postgres@notifications-db:5432/notifications' --name notifications --network notifications-service-network --ip 172.23.0.2 $REGISTRY_REPO/$NOTIFICATIONS:$TAG"
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d --restart always -e 'POSTGRES_USER=postgres' -e 'POSTGRES_PASSWORD=postgres' -p 5436:5432 --name notifications-db --network notifications-service-network --ip 172.23.0.3 $REGISTRY_REPO/$NOTIFICATIONS_DB:$TAG"
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d --restart always -e 'NODE_ENV=development' -e 'DATABASE_URL=postgres://postgres:postgres@notifications-db:5432/notifications' --name notifications --network notifications-service-network --ip 172.23.0.2 $REGISTRY_REPO/$NOTIFICATIONS:$TAG"
 
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network connect onelike-network --ip 172.18.0.12 notifications'
 

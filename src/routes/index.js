dfb1e2c1424b21a59344cc4d4e7c04e8332148b5
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2019-01-18 11:47:32 -0300

    Swagger definition

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,13 +5,8 @@ module.exports = (app, io) => {
   const notificationsController = require('../controllers/notifications')(io);
 
   app.get('/notifications-service/health', healthController.health);
-  app.post('/notifications/send', notificationsController.send);
-  app.get('/notifications-service/test', (req, res) => {
-    res.sendFile(path.join(__dirname+'/../tests/index.html'));
-  });
-
   app.get('/notifications/:hash', notificationsController.get);
-
+  app.post('/notifications/send', notificationsController.send);
   app.delete('/notifications/:id', notificationsController.remove);
 
 };
